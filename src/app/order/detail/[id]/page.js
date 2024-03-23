ae0550ea7386e68145b4735307beef1b029191dc
o220s <pjs47792332@gmail.com>
2024-03-23 11:40:14 +0900

    feat : xlsx 다운로드 기능구현

--- src/app/order/detail/[id]/page.js
+++ src/app/order/detail/[id]/page.js
@@ -1,3 +1,4 @@
+'use client'
 import OrderDetailPage from '@/components/orderProductList'
 
 export async function getData({ params: { id } }) {
@@ -7,10 +8,35 @@ export async function getData({ params: { id } }) {
   }
 }
 
+async function getXlsx(id) {
+  const URL = `http://localhost:8080/api/store/order/download/${id}`
+  const response = await fetch(URL)
+  if (!response.ok) {
+    throw new Error('엑셀 파일 다운로드에 실패했습니다.')
+  }
+  const blob = await response.blob()
+  const url = window.URL.createObjectURL(blob)
+  const a = document.createElement('a')
+  a.href = url
+  a.download = `Product order-${id}.xlsx`
+  document.body.appendChild(a)
+  a.click()
+  a.remove()
+  window.URL.revokeObjectURL(url)
+}
+
 export default async function OrderDetail({ params: { id } }) {
+  const handleXlsxClick = async () => {
+    try {
+      await getXlsx(id)
+    } catch (error) {
+      console.log(error)
+    }
+  }
   return (
     <div className="flex flex-col bg-gray-100 py-16">
       <OrderDetailPage id={id} />
+      <button onClick={handleXlsxClick}>발주서다운로드</button>
     </div>
   )
 }
