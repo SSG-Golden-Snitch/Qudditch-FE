2bf503ca72bac2b10bebc03409536195f0f9781e
Kim JinSeon <kwlstjs00@gmail.com>
2024-03-25 17:39:23 +0900

    feat: dayjs 라이브러리 추가 및 관련 코드 수정

--- src/components/customTable.js
+++ src/components/customTable.js
@@ -1,34 +1,46 @@
 'use client'
-
 import { Table, TableHeadCell } from 'flowbite-react'
+import { AiOutlineDownload } from 'react-icons/ai'
 
-export function CustomTable({ data, header, rowClick }) {
+export function CustomTable({ data, header }) {
   return (
-    <div>
-      <Table className="w-full items-center justify-center text-center text-xs">
+    <div className=" w-128 px-3">
+      <Table className="w-[calc(100vw-200px)] items-center justify-center text-center text-xs">
         <Table.Head>
           {header.map((h, index) => (
             <TableHeadCell
               key={index}
               className="text-m whitespace-nowrap text-gray-900 dark:text-white"
             >
-              {h['label']}
+              {h.label}
             </TableHeadCell>
           ))}
         </Table.Head>
         <Table.Body className="divide-y">
           {data.map((d, index) => (
             <Table.Row
-              onClick={() => rowClick(d.id)}
               key={index}
-              className="bg-white dark:border-gray-700 dark:bg-gray-800"
+              className="items-center bg-white dark:border-gray-700 dark:bg-gray-800"
             >
               {header.map((h, subIndex) => (
                 <Table.Cell
                   key={subIndex}
-                  className="whitespace-nowrap font-medium text-gray-900 dark:text-white"
+                  className="items-cetner whitespace-nowrap text-center font-medium text-gray-900 dark:text-white"
+                  style={{ position: 'relative' }} // 아이콘 위치 지정
                 >
-                  {d[h['col_name']]}
+                  {h.col_name === 'inputAt' ? d[h.col_name].split('T')[0] : d[h.col_name]}
+                  {h.col_name === 'download' && ( // 다운로드 컬럼에 대한 조건 추가
+                    <div
+                      style={{
+                        position: 'absolute',
+                        top: '50%',
+                        left: '50%',
+                        transform: 'translate(-50%, -50%)',
+                      }}
+                    >
+                      <AiOutlineDownload />
+                    </div>
+                  )}
                 </Table.Cell>
               ))}
             </Table.Row>
