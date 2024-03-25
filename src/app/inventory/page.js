529d95a8b983a6851deef9c84be00f8211f27aa7
Kim JinSeon <kwlstjs00@gmail.com>
2024-03-25 11:15:37 +0900

    fix: 페이지에서 카테고리 선택 기능 수정

--- src/app/inventory/page.js
+++ src/app/inventory/page.js
@@ -2,13 +2,12 @@
 
 import { CustomTable } from '@/components/customTable'
 import { useEffect, useState } from 'react'
-import { Pagination } from 'flowbite-react'
+import { Pagination, Select } from 'flowbite-react'
 
 export default function Stock() {
   const [pagination, setPagination] = useState({
     paginationParam: {
       page: 1,
-      recordSize: 10,
     },
     totalPageCount: 0,
     startPage: 0,
@@ -19,15 +18,15 @@ export default function Stock() {
   const [storeStock, setStoreStock] = useState([])
   const [isLoading, setIsLoading] = useState(false)
   const [error, setError] = useState(null)
+  const [categoryId, setCategoryId] = useState(0)
 
-  const handleStoreStock = async (page = 1, recordSize = 10) => {
+  const handleStoreStock = async (page = 1, categoryId = 0) => {
+    setError(null)
     setIsLoading(true)
 
     const storeStockReqUrl = new URL(
-      `http://localhost:8080/api/store/stock?page=${page}&recordSize=${recordSize}`,
+      `http://localhost:8080/api/store/stock?page=${page}&categoryId=${categoryId}`,
     )
-
-    // storeStockReqUrl.searchParams.append('categoryId', 1)
 
     await fetch(storeStockReqUrl, {
       method: 'GET',
@@ -39,11 +38,15 @@ export default function Stock() {
     })
       .then((res) => res.json())
       .then((res) => {
-        setStoreStock(res['data'])
-        setPagination(res['pagination'])
+        if (res['status'] === 'fail') {
+          throw new Error(res['message'])
+        } else {
+          setStoreStock(res['data'])
+          setPagination(res['pagination'])
+        }
       })
       .catch((error) => {
-        setError(error)
+        setError(error.message)
       })
       .finally(() => {
         setIsLoading(false)
@@ -62,38 +65,67 @@ export default function Stock() {
     handleStoreStock(page)
   }
 
-  if (isLoading) return <div className="h-screen bg-gray-100 p-6 py-16 ">Loading...</div> // 로딩 중 화면
+  const handleCategoryChange = (event) => {
+    const selectedCategoryId = parseInt(event.target.value)
+    console.log(selectedCategoryId)
+    handleStoreStock(1, selectedCategoryId)
 
-  if (error) return <div>Error: {error}</div>
+    setCategoryId(selectedCategoryId)
+  }
+
+  if (isLoading) return <div className="h-screen bg-gray-100 p-6 py-16 ">Loading...</div>
+
+  //   if (error) return <div className="h-screen bg-gray-100 p-6 py-16 ">{error}</div>
 
   return (
-    <div className="felx grid h-screen flex-col bg-gray-100 py-16">
-      <div>
-        <h1 className="text-center text-2xl font-bold">category</h1>
+    <div className="flex h-screen flex-col bg-gray-100 py-10">
+      <div className="px-12 pb-5 text-right">
+        <Select
+          className="w-32"
+          id="categories"
+          required
+          onChange={handleCategoryChange}
+          value={categoryId}
+        >
+          <option value={0}>전체</option>
+          <option value={1}>과자</option>
+          <option value={2}>아이스크림</option>
+          <option value={3}>초콜릿</option>
+          <option value={4}>젤리/푸딩</option>
+          <option value={5}>시리얼</option>
+          <option value={6}>음료수</option>
+          <option value={7}>생수</option>
+          <option value={8}>우유</option>
+          <option value={9}>라면</option>
+        </Select>
       </div>
       <div className="flex flex-col items-center">
-        <CustomTable
-          data={storeStock}
-          pagination={pagination}
-          header={[
-            { label: 'category', col_name: 'categoryName' },
-            { label: 'brand', col_name: 'brand' },
-            { label: 'name', col_name: 'productName' },
-            { label: 'price', col_name: 'productPrice' },
-            { label: 'quantity', col_name: 'qty' },
-            { label: 'position', col_name: 'position' },
-            { label: 'expirated', col_name: 'expiredAt' },
-            { label: 'edit', col_name: 'edit' },
-          ]}
-        />
-        {pagination && (
-          <div className="items-center justify-center ">
-            <Pagination
-              currentPage={pagination.paginationParam.page}
-              totalPages={pagination.totalPageCount}
-              onPageChange={(page) => handlePage(page)}
+        {error ? (
+          <div className="text-red-500">{error}</div>
+        ) : (
+          <>
+            <CustomTable
+              data={storeStock}
+              pagination={pagination}
+              header={[
+                { label: 'category', col_name: 'categoryName' },
+                { label: 'brand', col_name: 'brand' },
+                { label: 'name', col_name: 'productName' },
+                { label: 'price', col_name: 'productPrice' },
+                { label: 'quantity', col_name: 'qty' },
+                { label: 'position', col_name: 'position' },
+                { label: 'expirated', col_name: 'expiredAt' },
+                { label: 'edit', col_name: 'edit' },
+              ]}
             />
-          </div>
+            <div className="items-center justify-center">
+              <Pagination
+                currentPage={pagination.paginationParam.page}
+                totalPages={pagination.totalPageCount}
+                onPageChange={(page) => handlePage(page)}
+              />
+            </div>
+          </>
         )}
       </div>
     </div>
