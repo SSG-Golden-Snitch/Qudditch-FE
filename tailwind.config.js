6cc0be52731438b38febc7d68bffa0a29e828175
1min <dnjsxmf@naver.com>
2024-04-03 13:11:57 +0900

    fix:datepicker 달력 설정 누락으로 인해, 달력 레이아웃 깨져 서 tailwind.config.js 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{js,ts,jsx,tsx,mdx}', 'node_modules/flowbite-react/lib/esm/**/*.js'],
+  content: [
+    './src/**/*.{js,ts,jsx,tsx,mdx}',
+    'node_modules/flowbite-react/lib/esm/**/*.js',
+    './node_modules/tailwind-datepicker-react/dist/**/*.js',
+  ],
   theme: {
     extend: {},
   },
