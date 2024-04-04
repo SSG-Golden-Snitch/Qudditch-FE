6a9e6a33c9f62a15e35149513cfad55105fbb4e8
wonlog <bko0108@naver.com>
2024-04-04 16:30:04 +0900

    feat 장바구니 조회(/store-select/cart)

--- src/app/(app)/store-select/cart/page.js
+++ src/app/(app)/store-select/cart/page.js
@@ -2,11 +2,12 @@
 
 import React, { useState, useEffect } from 'react'
 import { fetchExtended } from '@/utils/fetchExtended'
+import { Button } from 'flowbite-react'
 
 const CartPage = () => {
   const [cartItems, setCartItems] = useState([])
-  const [userStoreId, setUserStoreId] = useState([])
-  const [productId, setProductId] = useState([])
+  // const [userStoreId, setUserStoreId] = useState([])
+  // const [productId, setProductId] = useState([])
   const [message, setMessage] = useState('')
 
   useEffect(() => {
@@ -14,31 +15,44 @@ const CartPage = () => {
     fetchCartItems()
   }, [])
 
-  const params = {
-    userStoreId: userStoreId,
-    productId: productId,
-  }
+  // 장바구니 조회 API
+  const fetchCartItems = async (userCustomerId) => {
+    // const params = {
+    //   storeId: storeId,
+    //   productId: productId,
+    // }
 
-  const fetchCartItems = async () => {
-    // 장바구니 조회 API
-    const queryString = new URLSearchParams(params).toString()
-    const endpoint = `/api/cart/items?${queryString}`
+    const queryString = new URLSearchParams(userCustomerId).toString()
+    const endpoint = `/api/cart?${queryString}`
 
-    const response = await fetchExtended(endpoint, {
-      method: 'GET',
-    })
-
-    if (response.ok) {
-      const data = await response.json()
-      setCartItems(data)
-    } else {
-      setMessage('품목 정보를 불러오는 데 실패하였습니다')
+    try {
+      const response = await fetchExtended(endpoint, {
+        method: 'GET',
+      })
+      if (response.ok) {
+        const data = await response.json()
+        setCartItems(data)
+      } else {
+        throw new Error('장바구니 목록을 불러오는데 실패했습니다.')
+      }
+    } catch (error) {
+      setMessage(error.message)
     }
   }
 
   return (
     <div className="p-4">
-      <h1 className="mb-4 text-2xl font-bold">장바구니</h1>
+      <h2 className="mb-4 text-2xl font-bold">장바구니</h2>
+      {message && <p>{message}</p>}
+      {cartItems.map((item) => (
+        <div key={item.productId}>
+          <span>
+            {item.name} - {item.qty}개
+          </span>
+          {/* 추가 버튼 예시 (상품 ID는 예시로 넣은 것입니다.) */}
+          <Button onClick={() => cartItems(item.productId)}>장바구니에 추가</Button>
+        </div>
+      ))}
     </div>
   )
 }
