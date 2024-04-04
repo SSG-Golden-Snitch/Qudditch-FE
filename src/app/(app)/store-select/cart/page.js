34342941ee56fd1f43b711bf21e1bde69f131932
wonlog <bko0108@naver.com>
2024-04-04 17:51:28 +0900

    style 장바구니 레이아웃 변경중

--- src/app/(app)/store-select/cart/page.js
+++ src/app/(app)/store-select/cart/page.js
@@ -3,6 +3,7 @@
 import React, { useState, useEffect } from 'react'
 import { fetchExtended } from '@/utils/fetchExtended'
 import { Button } from 'flowbite-react'
+import { IoMdClose } from 'react-icons/io'
 
 const CartPage = () => {
   const [cartItems, setCartItems] = useState([])
@@ -40,17 +41,40 @@ const CartPage = () => {
     }
   }
 
+  // 세 자리마다 , 표시
+  const formatNumber = (number) => {
+    if (!number) return '0'
+    return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',') // 세자리마다 , 표시 추가
+  }
+
   return (
     <div className="p-4">
       <h2 className="mb-4 text-2xl font-bold">장바구니</h2>
       {message && <p>{message}</p>}
+
+      <h3 className="mb-4 border-b-2">결제 상품</h3>
+
       {cartItems.map((item) => (
-        <div key={item.productId}>
+        <div key={item.productId} className="flex items-center justify-between border-y-2 py-2">
+          <div class="flex items-center">
+            <input
+              checked
+              id="checked-checkbox"
+              type="checkbox"
+              value=""
+              class="h-4 w-4 rounded border-gray-300 bg-gray-100 text-blue-600 focus:ring-2 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:ring-offset-gray-800 dark:focus:ring-blue-600"
+            />
+          </div>
+          <img src={item.image} alt={item.name} className="h-20 w-20 object-cover" />
           <span>
             {item.name} - {item.qty}개
           </span>
-          {/* 추가 버튼 예시 (상품 ID는 예시로 넣은 것입니다.) */}
-          <Button onClick={() => cartItems(item.productId)}>장바구니에 추가</Button>
+
+          <h4>{formatNumber(item.price)}</h4>
+
+          <Button onClick={() => removeItemFromCart(item.productId)}>
+            <IoMdClose />
+          </Button>
         </div>
       ))}
     </div>
