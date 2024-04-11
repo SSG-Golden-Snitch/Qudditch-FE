d2f347aa571789ee4923f5a832505b2603335feb
HanDaWoon <bookpia.han@gmail.com>
2024-04-11 20:01:37 +0900

    fix: Main 오류 수정

--- src/components/MobileMain.js
+++ src/components/MobileMain.js
@@ -181,8 +181,6 @@ const MobileMain = () => {
       </div>
       <div className="px-7 pt-5">
         <div className="text-xm flex w-full max-w-4xl justify-around gap-0">
-          <Link href="/access" passHref>
-            <div className="flex flex-col items-center text-center active:bg-gray-100">
           <Link href="/m/access" passHref>
             <div className="flex flex-col items-center text-center active:bg-gray-100">
               <div className="text-6xl">
@@ -191,7 +189,6 @@ const MobileMain = () => {
               <p>매장출입</p>
             </div>
           </Link>
-          <Link href="/map" passHref>
           <Link href="/m/map" passHref>
             <div className="flex flex-col items-center text-center active:bg-gray-100">
               <div className="text-6xl">
@@ -200,8 +197,6 @@ const MobileMain = () => {
               <p>매장찾기</p>
             </div>
           </Link>
-          <Link href="chatbot" passHref>
-            <div className="flex flex-col items-center text-center active:bg-gray-100">
           <Link href="/m/chatbot" passHref>
             <div className="flex flex-col items-center text-center active:bg-gray-100">
               <div className="text-6xl">
