1380bbbbd81967861ea939c1c51101f446331c88
wonlog <bko0108@naver.com>
2024-04-12 23:09:25 +0900

    fix QR scan 루프 해결 (제품이 2번씩 인식되는 문제 해결x)

--- src/components/QRCodeScanner.js
+++ src/components/QRCodeScanner.js
@@ -1,34 +1,44 @@
-// QRCodeScanner.jsx
 import React, { useEffect, useState } from 'react'
-import { BrowserQRCodeReader } from '@zxing/library'
+import { BrowserQRCodeReader, NotFoundException } from '@zxing/library'
 
 const QRCodeScanner = ({ onScan }) => {
-  const [videoInputDevices, setVideoInputDevices] = useState([])
-
   useEffect(() => {
     const codeReader = new BrowserQRCodeReader()
-    codeReader.listVideoInputDevices().then((videoInputDevices) => {
-      setVideoInputDevices(videoInputDevices)
-      if (videoInputDevices.length > 0) {
-        const selectedDeviceId = videoInputDevices[0].deviceId
-        codeReader.decodeFromVideoDevice(selectedDeviceId, 'video', (result, err) => {
-          if (result) {
-            onScan(result.getText())
-            codeReader.reset()
-          }
-          if (err) {
-            console.error(err)
-          }
-        })
-      }
-    })
+    let selectedDeviceId
+
+    const startScan = () => {
+      codeReader.decodeFromVideoDevice(selectedDeviceId, 'video', (result, err) => {
+        if (result) {
+          onScan(result.getText())
+          codeReader.reset() // 성공 후 즉시 리셋
+        } else if (err && !(err instanceof NotFoundException)) {
+          console.error('Scanning error:', err)
+        }
+      })
+    }
+
+    codeReader
+      .listVideoInputDevices()
+      .then((videoInputDevices) => {
+        if (videoInputDevices.length > 0) {
+          selectedDeviceId = videoInputDevices[0].deviceId
+          startScan()
+        }
+      })
+      .catch((error) => console.error('Error listing video devices:', error))
 
     return () => {
       codeReader.reset()
+      // Optionally stop the video stream if it's still running
+      if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+        navigator.mediaDevices.getUserMedia({ video: true }).then((stream) => {
+          stream.getTracks().forEach((track) => track.stop())
+        })
+      }
     }
   }, [onScan])
 
-  return <video id="video" style={{ width: '100%' }} />
+  return <video id="video" style={{ width: '100%' }} autoPlay />
 }
 
 export default QRCodeScanner
