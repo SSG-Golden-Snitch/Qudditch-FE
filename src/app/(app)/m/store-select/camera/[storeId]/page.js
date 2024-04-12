abfaffe8049586f8c6cb9a0119bc008df66dc5d1
wonlog <bko0108@naver.com>
2024-04-12 11:40:57 +0900

    페이지 변경 (store-select)

--- src/app/(app)/m/store-select/camera/[storeId]/page.js
+++ src/app/(app)/m/store-select/camera/[storeId]/page.js
@@ -1,7 +1,6 @@
 'use client'
 
 import React, { useEffect, useRef, useState } from 'react'
-import { FaCircle } from 'react-icons/fa'
 import { FiShoppingCart } from 'react-icons/fi'
 import Link from 'next/link'
 
@@ -22,54 +21,76 @@ const CameraPage = () => {
   // 장바구니 카운트 추가
   const [cartItemCount, setCartItemCount] = useState(0)
 
+  // 스캔 중복 방지
+  const [isScanning, setIsScanning] = useState(false)
+
   // selectedStore 상태 추가
   const [selectedStore, setSelectedStore] = useState(null)
 
-  // useEffect(() => {
-  //   // storeId가 유효한 경우에만 selectedStore 상태를 업데이트합니다.
-  //   if (storeId) {
-  //     setSelectedStore(storeId);
-  //   }
-  // }, [storeId]);
-
   // QR 스캔 기능
   const handleScan = async (productId) => {
-    const scanProduct = async (attempt = 0) => {
-      const maxAttempts = 5
-      if (attempt >= maxAttempts) {
-        console.error('최대 시도 횟수에 도달했습니다.')
-        return
-      }
+    // const scanProduct = async (attempt = 0) => {
+    //   const maxAttempts = 5
+    //   if (attempt >= maxAttempts) {
+    //     console.error('최대 시도 횟수에 도달했습니다.')
+    //     return
+    //   }
 
-      const usedPoint = 0 // 사용할 포인트 (예시로 0을 설정)
+    //   const usedPoint = 0 // 사용할 포인트 (예시로 0을 설정)
 
-      const requestData = {
-        storeId,
-        productId,
-        usedPoint,
-      }
+    //   const requestData = {
+    //     storeId,
+    //     productId,
+    //     usedPoint,
+    //   }
 
-      try {
-        const response = await fetchExtended(`/api/cart`, {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify(requestData),
-        })
+    //   try {
+    //     const response = await fetchExtended(`/api/cart`, {
+    //       method: 'POST',
+    //       headers: {
+    //         'Content-Type': 'application/json',
+    //       },
+    //       body: JSON.stringify(requestData),
+    //     })
 
-        if (!response.ok) {
-          throw new Error('Failed to add item to cart.')
-        }
-        console.log('Item added to cart successfully.')
-        setCartItemCount((prevCount) => prevCount + 1) // 장바구니 카운트 증가
-      } catch (error) {
-        console.error('Error adding item to cart:', error)
-        setTimeout(() => scanProduct(attempt + 1), 100) // 재시도
-      }
+    //     if (!response.ok) {
+    //       throw new Error('Failed to add item to cart.')
+    //     }
+    //     console.log('Item added to cart successfully.')
+    //     setCartItemCount((prevCount) => prevCount + 1) // 장바구니 카운트 증가
+    //   } catch (error) {
+    //     console.error('Error adding item to cart:', error)
+    //     setTimeout(() => scanProduct(attempt + 1), 100) // 재시도
+    //   }
+    // }
+
+    // scanProduct() // 최초 스캔 시도
+
+    if (isScanning) return // 이미 스캔 중이면 무시
+    setIsScanning(true) // 스캔 시작
+
+    try {
+      const response = await fetchExtended(`/api/cart`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          storeId,
+          productId,
+          usedPoint: 0,
+        }),
+      })
+
+      if (!response.ok) throw new Error('Failed to add item to cart.')
+
+      setCartItemCount((prevCount) => prevCount + 1) // 성공 시 카운트 증가
+      console.log('Item added to cart successfully.')
+    } catch (error) {
+      console.error('Error adding item to cart:', error)
     }
 
-    scanProduct() // 최초 스캔 시도
+    setTimeout(() => setIsScanning(false), 5000) // 5초 후에 다시 스캔 가능
   }
 
   return (
@@ -77,20 +98,6 @@ const CameraPage = () => {
       <h1 className="mb-4 text-2xl font-bold">QR 인식</h1>
       <div className="relative">
         <QRCodeScanner onScan={handleScan} />
-        {/* <FaCircle
-          onClick={takePhoto}
-          style={{
-            position: 'absolute',
-            bottom: '10%', // 비디오 하단에서 10%의 위치에 배치
-            left: '50%', // 컨테이너 중앙에 배치
-            transform: 'translateX(-50%)', // 왼쪽으로 50% 이동하여 중앙 정렬
-            fontSize: '48px', // 아이콘 크기
-            color: 'white', // 아이콘 색상
-            border: '2px solid red', // 테두리 색상
-            borderRadius: '50%', // 원형 테두리
-            cursor: 'pointer',
-          }}
-        /> */}
 
         <Link href={`/m/store-select/cart`}>
           <FiShoppingCart
@@ -103,8 +110,8 @@ const CameraPage = () => {
               color: 'white',
             }}
           />
+          <span>{cartItemCount}</span>
         </Link>
-        <span>{cartItemCount}</span>
       </div>
       {image && <img src={image} className="mt-4 rounded-lg" alt="Snapshot" />}
       <canvas ref={canvasRef} style={{ display: 'none' }}></canvas>
@@ -118,3 +125,6 @@ export default CameraPage
 // 2. QR 코드 스캔
 // 3. 스캔 데이터 처리
 // 4. 장바구니 페이지 업데이트
+
+// 오류
+// 한 번만 인식하는 게 아니라, 연속으로 인식됨
