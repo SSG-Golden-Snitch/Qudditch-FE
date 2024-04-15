30b2aa0d47db473a816bfda5956886c5d9578172
o220s <pjs47792332@gmail.com>
2024-04-15 13:37:42 +0900

    feat : 마커 클릭 시 지도이동 , 각 기능 전부 Loading 컴포넌트 추가

--- src/components/inventoryList.js
+++ src/components/inventoryList.js
@@ -9,6 +9,7 @@ import { Autocomplete, Loader, View } from '@aws-amplify/ui-react'
 import Image from 'next/image'
 
 import { useTheme } from '@aws-amplify/ui-react'
+import Loading from './ui/Loading'
 
 export async function getData(id) {
   const response = await fetchExtended(`/api/store/location/stock?userStoreId=${id}`)
@@ -83,7 +84,7 @@ export default function LocationStockPage({ id }) {
   }, [id])
 
   if (loading) {
-    return <div>Loading...</div>
+    return <Loading />
   }
 
   if (!data || !data.list || data.list.length === 0) {
