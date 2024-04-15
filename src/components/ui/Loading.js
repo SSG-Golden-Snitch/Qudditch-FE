63db06bce8e1f50f257fc7ed5d1f9dba837d570b
o220s <pjs47792332@gmail.com>
2024-04-15 14:43:01 +0900

    fix : 로딩중 최종

--- src/components/ui/Loading.js
+++ src/components/ui/Loading.js
@@ -1,11 +1,26 @@
 const Loading = () => {
   return (
     <div className="flex h-screen items-center justify-center">
-      <img
-        class="h-20 w-20 animate-spin"
-        src="https://www.svgrepo.com/show/448500/loading.svg"
-        alt="Loading icon"
-      />
+      <svg
+        class="h-16 w-16 animate-spin"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+      >
+        <circle
+          class="opacity-25"
+          cx="12"
+          cy="12"
+          r="10"
+          stroke="currentColor"
+          stroke-width="4"
+        ></circle>
+        <path
+          class="opacity-75"
+          fill="currentColor"
+          d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+        ></path>
+      </svg>
     </div>
   )
 }
