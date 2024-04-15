a07700b34a67c7c5c64abaebf9fbf6aea17d2645
Kim JinSeon <kwlstjs00@gmail.com>
2024-04-15 20:38:08 +0900

    feat: 스플래시 화면 추가

--- src/app/(app)/m/(mobile-nav)/page.js
+++ src/app/(app)/m/(mobile-nav)/page.js
@@ -1,9 +1,29 @@
+'use client'
+
+import { useState, useEffect } from 'react'
+import Splash from '@/components/Splash'
 import MobileMain from '@/components/MobileMain'
 
 const Main = () => {
+  const [showSplash, setShowSplash] = useState(true)
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setShowSplash(false)
+    }, 2000)
+
+    return () => clearTimeout(timeout)
+  }, [])
+
   return (
     <div>
-      <MobileMain />
+      {showSplash ? (
+        <Splash />
+      ) : (
+        <div>
+          <MobileMain />
+        </div>
+      )}
     </div>
   )
 }
