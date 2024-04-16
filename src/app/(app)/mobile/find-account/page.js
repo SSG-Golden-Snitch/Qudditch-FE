f93b879fbe8c383a4b647d17fa876bdd2b593446
o220s <pjs47792332@gmail.com>
2024-04-16 19:28:27 +0900

    feat : 모바일 로그인화면, 아이디찾기 화면 구현

--- src/app/(app)/mobile/find-account/page.js
+++ src/app/(app)/mobile/find-account/page.js
@@ -2,7 +2,7 @@
 //http://localhost:3000/mobile/find-account
 'use client'
 
-import { Alert, Button, Label, TextInput } from 'flowbite-react'
+import { Alert, Label } from 'flowbite-react'
 import { useRouter } from 'next/navigation'
 import { useState } from 'react'
 import { fetchExtended } from '@/utils/fetchExtended'
@@ -115,36 +115,61 @@ export default function FindAccountPage() {
   }
 
   return (
-    <div className="container mx-auto p-4">
-      <div className="mb-4">
-        {message && <Alert color="green">{message}</Alert>}
-        {error && <Alert color="red">{error}</Alert>}
-      </div>
-      <div className="flex flex-col gap-4">
-        <div>
-          <Label htmlFor="name">이름</Label>
-          <TextInput
-            id="name"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-            placeholder="이름을 입력해주세요"
-          />
-          <Button onClick={handleFindEmail}>이메일 찾기</Button>
+    <section className="flex h-screen max-w-screen-xl items-center justify-center bg-white">
+      <div className="flex flex-col items-center justify-center px-6 py-8 md:h-screen lg:py-0">
+        <span className="pb-1 text-gray-500">딜리셔스 아이디어</span>
+        <img className="h-50 w-50 mr-2" src="/WebLogo.svg" alt="logo"></img>
+
+        <div className="flex flex-col gap-4 p-5">
+          <div>
+            <Label htmlFor="name">이름으로 Email 찾기</Label>
+            <input
+              id="name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              placeholder="이름을 입력해주세요"
+              className=" w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-gray-900 focus:border-gray-600 focus:ring-gray-600 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-gray-500 dark:focus:ring-gray-500 sm:text-sm"
+            />
+          </div>
+          <button
+            onClick={handleFindEmail}
+            className=" w-full rounded-lg bg-amber-400 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-amber-400 focus:outline-none focus:ring-4 focus:ring-amber-300 dark:bg-amber-600"
+          >
+            이메일찾기
+          </button>
+          <div>
+            <Label htmlFor="email">Email로 비밀번호 재설정하기</Label>
+            <input
+              id="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              placeholder="이메일을 입력해주세요"
+              className=" w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-gray-900 focus:border-gray-600 focus:ring-gray-600 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-gray-500 dark:focus:ring-gray-500 sm:text-sm"
+            />
+          </div>
+          <button
+            onClick={handleResetPassword}
+            className=" w-full rounded-lg bg-amber-400 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-amber-400 focus:outline-none focus:ring-4 focus:ring-amber-300 dark:bg-amber-600"
+          >
+            인증메일발송
+          </button>
         </div>
-        <div>
-          <Label htmlFor="email">이메일</Label>
-          <TextInput
-            id="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            placeholder="이메일을 입력해주세요"
-          />
-          <Button onClick={handleResetPassword}>비밀번호 재설정 이메일 받기</Button>
+
+        <div className="mb-4">
+          {message && <Alert color="green">{message}</Alert>}
+          {error && <Alert color="red">{error}</Alert>}
+        </div>
+        <div className="flex w-full justify-end">
+          <button
+            type="button"
+            color="gray"
+            onClick={() => router.push('/mobile/login')}
+            className="mb-2 me-2 rounded-full border border-gray-200 bg-white px-5 py-2.5 text-sm font-medium text-gray-900 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:outline-none focus:ring-4 focus:ring-gray-100 dark:border-gray-600 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white dark:focus:ring-gray-700"
+          >
+            돌아가기
+          </button>
         </div>
       </div>
-      <Button color="gray" onClick={() => router.push('/mobile/login')}>
-        로그인 화면으로 돌아가기
-      </Button>
-    </div>
+    </section>
   )
 }
