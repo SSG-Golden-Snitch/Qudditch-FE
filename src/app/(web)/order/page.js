1eef0031b7684fc80376fc994535f08bb21e7e92
o220s <pjs47792332@gmail.com>
2024-04-17 14:41:32 +0900

    fix : 발주 버튼 수정

--- src/app/(web)/order/page.js
+++ src/app/(web)/order/page.js
@@ -69,12 +69,31 @@ export default function OrderListPage() {
         </Table.Body>
       </Table>
       <br />
-      <button
-        className="rounded border border-gray-400 bg-white px-5 py-2 font-semibold text-gray-800 shadow hover:bg-gray-100"
+      <a
         onClick={handleInsertClick}
+        class="group relative inline-flex items-center justify-center overflow-hidden rounded-full border-2 border-gray-500 p-3 px-4 py-2 font-medium text-gray-600 shadow-md transition duration-300 ease-out"
       >
-        발주등록
-      </button>
+        <span class="ease absolute inset-0 flex h-full w-full -translate-x-full items-center justify-center bg-gray-500 text-white duration-300 group-hover:translate-x-0">
+          <svg
+            class="h-6 w-6"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M14 5l7 7m0 0l-7 7m7-7H3"
+            ></path>
+          </svg>
+        </span>
+        <span class="ease absolute flex h-full w-full transform items-center justify-center text-gray-500 transition-all duration-300 group-hover:translate-x-full">
+          발주등록
+        </span>
+        <span class="invisible relative">발주등록</span>
+      </a>
       <div className="relative flex items-center justify-center pt-10">
         <Pagination
           currentPage={orderData.pagination.paginationParam.page}
