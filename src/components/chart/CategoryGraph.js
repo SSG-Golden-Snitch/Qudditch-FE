3d2bc7133671b17056b558f36cc934a471b072f5
1min <dnjsxmf@naver.com>
2024-04-17 21:28:58 +0900

    design: 카테고리 차트 색상변경

--- src/components/chart/CategoryGraph.js
+++ src/components/chart/CategoryGraph.js
@@ -120,7 +120,13 @@ const CategoryGraph = ({ dateInput }) => {
         {
           label: '매출',
           data: productDataSet,
-          backgroundColor: doughnutChartColor,
+          backgroundColor: [
+            'rgba(179, 140, 180, 0.8)',
+            'rgba(183, 145, 140, 0.8)',
+            'rgba(197, 164, 138, 0.8)',
+            'rgba(221, 198, 123, 0.8)',
+            'rgba(253, 224, 71, 0.8)',
+          ],
           hoverOffset: 4,
         },
       ],
