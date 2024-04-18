28e6e6b69595eb03a2c6914dc34b3f7b13183999
shinminchan <althwkd18@naver.com>
2024-04-18 13:01:40 +0900

    fix: 채팅위치 수정

--- src/app/(app)/m/chatbot/page.js
+++ src/app/(app)/m/chatbot/page.js
@@ -183,87 +183,89 @@ function Chatbot() {
   }
 
   return (
-    <div className="chat-app mx-auto flex h-screen max-w-md flex-col rounded-lg  bg-gray-200 font-sans">
-      <header className="chat-header flex items-center justify-between  bg-gray-800 p-2 text-white">
-        <div className="flex">
-          <Image src={dobbyImage} alt="도비" className="h-8 w-8 rounded-full" />
-          <h1 className="pl-3 text-lg font-semibold">Dobby Chat</h1>
-        </div>
-        <button className="home-button text-xl" aria-label="Home">
-          <Link href="/m">
-            <HiOutlineHome />
-          </Link>
-        </button>
-      </header>
-      <div className="chat-messages flex-1 overflow-y-auto p-5">
-        {messages.map((message, index) => (
-          <div key={index}>
-            {/* 발신자의 이름이 있는 경우에만 표시 */}
-            {message.sender && (
-              <div
-                className="sender-info mb-2 font-medium text-gray-600"
-                style={{ textAlign: message.sender === '나' ? 'right' : 'left' }}
-              >
-                {/* 발신자가 '나'이면 그대로 표시, 그 외에는 이미지로 대체 */}
-                {message.sender === '나' ? (
-                  message.sender
-                ) : (
-                  <Image src={dobbyImage} alt="도비" className="h-8 w-8 rounded-full" />
-                )}
-              </div>
-            )}
-
-            <div
-              className={
-                message.sender === '나' ? 'mb-4 flex justify-end' : 'mb-4 flex justify-start'
-              }
-            >
-              <div className={message.sender === '나' ? 'message-right' : 'message-left'}>
+    <div className="min-h-full min-w-full">
+      <div className="chat-app mx-auto flex h-screen max-w-full flex-col rounded-lg  bg-gray-200 font-sans">
+        <header className="chat-header flex min-w-full items-center  justify-between bg-gray-800 p-2 text-white">
+          <div className="flex">
+            <Image src={dobbyImage} alt="도비" className="h-8 w-8 rounded-full" />
+            <h1 className="pl-3 text-lg font-semibold">Dobby Chat</h1>
+          </div>
+          <button className="home-button text-xl" aria-label="Home">
+            <Link href="/m">
+              <HiOutlineHome />
+            </Link>
+          </button>
+        </header>
+        <div className="chat-messages flex-1 overflow-y-auto p-5">
+          {messages.map((message, index) => (
+            <div key={index}>
+              {/* 발신자의 이름이 있는 경우에만 표시 */}
+              {message.sender && (
                 <div
-                  className={
-                    message.sender === '나'
-                      ? 'rounded-lg bg-yellow-100 p-3'
-                      : 'rounded-lg bg-gray-100 p-3'
-                  }
-                  style={{ whiteSpace: 'pre-line' }}
+                  className="sender-info mb-2 font-medium text-gray-600"
+                  style={{ textAlign: message.sender === '나' ? 'right' : 'left' }}
                 >
-                  <p className="text-gray-800">{message.text}</p>
-                  <span className="timestamp block text-right text-xs text-gray-500">
-                    {message.time}
-                  </span>
+                  {/* 발신자가 '나'이면 그대로 표시, 그 외에는 이미지로 대체 */}
+                  {message.sender === '나' ? (
+                    message.sender
+                  ) : (
+                    <Image src={dobbyImage} alt="도비" className="h-8 w-8 rounded-full" />
+                  )}
+                </div>
+              )}
+
+              <div
+                className={
+                  message.sender === '나' ? 'mb-4 flex justify-end' : 'mb-4 flex justify-start'
+                }
+              >
+                <div className={message.sender === '나' ? 'message-right' : 'message-left'}>
+                  <div
+                    className={
+                      message.sender === '나'
+                        ? 'rounded-lg bg-yellow-100 p-3'
+                        : 'rounded-lg bg-gray-100 p-3'
+                    }
+                    style={{ whiteSpace: 'pre-line' }}
+                  >
+                    <p className="text-gray-800">{message.text}</p>
+                    <span className="timestamp block text-right text-xs text-gray-500">
+                      {message.time}
+                    </span>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-        ))}
-        <div ref={messagesEndRef} />
-      </div>
-      <div className="chat-input flex items-center  bg-gray-300 p-3">
-        <button
-          onClick={listening ? stopListening : startListening}
-          className="mr-2 rounded-full bg-gray-500 px-4 py-2 text-white hover:bg-blue-600 focus:bg-blue-600 focus:outline-none"
-        >
-          {listening ? <BsStopCircle /> : <CiMicrophoneOn />}
-        </button>
-        <input
-          type="text"
-          value={inputValue}
-          onChange={(e) => setInputValue(e.target.value)}
-          onKeyDown={(e) => {
-            if (e.key === 'Enter') {
-              handleMessageSend()
-            }
-          }}
-          placeholder="Type a message..."
-          className="flex-grow rounded-full border border-gray-300 px-3 py-2 outline-none focus:border-blue-500"
-        />
-        <button
-          onTouchStart={handleMessageSend}
-          disabled={!inputValue} // inputValue가 비어있을 때 버튼을 비활성화합니다.
-          className="ml-2 rounded-full bg-gray-500 px-4 py-2 text-white hover:bg-blue-600 focus:bg-blue-600 focus:outline-none"
-        >
-          <VscSend />
-        </button>
+          ))}
+          <div ref={messagesEndRef} />
+        </div>
+        <div className="chat-input flex items-center  bg-gray-300 p-3 pb-7">
+          <button
+            onClick={listening ? stopListening : startListening}
+            className="mr-2 rounded-full bg-gray-500 px-4 py-2 text-white hover:bg-blue-600 focus:bg-blue-600 focus:outline-none"
+          >
+            {listening ? <BsStopCircle /> : <CiMicrophoneOn />}
+          </button>
+          <input
+            type="text"
+            value={inputValue}
+            onChange={(e) => setInputValue(e.target.value)}
+            onKeyDown={(e) => {
+              if (e.key === 'Enter') {
+                handleMessageSend()
+              }
+            }}
+            placeholder="Type a message..."
+            className="flex-grow rounded-full border border-gray-300 px-3 py-2 outline-none focus:border-blue-500"
+          />
+          <button
+            onTouchStart={handleMessageSend}
+            disabled={!inputValue} // inputValue가 비어있을 때 버튼을 비활성화합니다.
+            className="ml-2 rounded-full bg-gray-500 px-4 py-2 text-white hover:bg-blue-600 focus:bg-blue-600 focus:outline-none"
+          >
+            <VscSend />
+          </button>
+        </div>
       </div>
     </div>
   )
